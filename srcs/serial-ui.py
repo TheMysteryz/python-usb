ca13a0279d8cf4082791f8a59c303ea4b6a1af9a
TheMysteryz <zhuroland@hotmail.com>
2021-06-11 17:30:10 -0400

    img + program done v0.1

--- srcs/serial-ui.py
+++ srcs/serial-ui.py
@@ -1,15 +1,28 @@
 import PySimpleGUI as sg
+import serial
 import urllib.request, json
-import time
+import time, re, io, os
 
 # home
 #DATA_URL = "http://192.168.1.19:3000/getdata"
 # store
 DATA_URL = "http://192.168.1.12:3000/getdata"
 
+# define serial
+ser = serial.Serial()
+ser.port = '/dev/ttyACM0'
+ser.timeout = 1
+ser.open()
+sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
+
 # get datas from serv json
 with urllib.request.urlopen(DATA_URL) as url:
     datas = json.loads(url.read().decode())
+
+# func start truncate
+def truncate(num):
+    return re.sub(r'^(\d+\.\d{,2})\d*$',r'\1',str(num))
+# end func
 
 # func start compare data
 def cmp_data(q):
@@ -20,7 +33,8 @@ def cmp_data(q):
     for data in datas:
         code = format(data[0])
         label = format(data[1]).upper()
-        price = format(data[2]) + " €"
+        fprice = format(data[2])
+        price = truncate('{:<011}'.format(fprice)) + " €"
 
         if q in code or q in label:
 
@@ -28,7 +42,7 @@ def cmp_data(q):
             return [search, code, label, price]
             break
 
-    return search
+    return ["nofound", "Article non trouvé!", "Merci de réessayer ou\nde le signaler,", "A bientôt"]
 # end func
 
 # get screen width and height
@@ -42,13 +56,14 @@ aa = ""
 my_font = "Arial 58"
 my_font_small = "Arial 29"
 is_wait = False
+filename = r'~/python/img/triangle.png'
+print(filename)
 
 layout = [
     [sg.Text(SCAN_HERE[0], size=(30, 1), font=my_font, justification="center", background_color=BG_COLOR, key='-CODE-')],
     [sg.Text(SCAN_HERE[1], size=(30, 2), font=my_font, justification="center", background_color=BG_COLOR, key='-LABEL-')],
-    [sg.Text(SCAN_HERE[2], size=(6, 1), font=my_font, justification="center", background_color=BG_COLOR, text_color="#ff0", key='-PRICE-')],
-    [sg.Button("OK", button_color="#f00")],
-    [sg.Button("EXIT", button_color="#f00")]
+    [sg.Text(SCAN_HERE[2], size=(30, 1), font=my_font, justification="center", background_color=BG_COLOR, text_color="#ff0", key='-PRICE-')],
+    [sg.Image(filename, key='-IMAGE-')]
 ]
 
 # Create the window
@@ -61,7 +76,7 @@ window = sg.Window(
     no_titlebar = True,
     location = (0, 0),
     size = (w, h*2),
-    margins = (0, h/3),
+    margins = (0, h/4),
     keep_on_top = True
 ).Finalize()
 
@@ -75,15 +90,30 @@ while True:
         print(event)
 
     # inputs
-
-    if event == "OK" or "space" in event:
-        aa = "3660715013473"
+    if not is_wait:
+        while True:
+            sio.flush()
+            line = sio.readline()
+            if line != "":
+                aa = line.strip()
+                break
+    # if event == "OK" or "space" in event:
+    #     aa = "3660715013473"
 
     # search input in datas
     [text, code, label, price] = cmp_data(aa)
 
     # when data founds
-    if text:
+    if text == "nofound":
+        # add 3 sec on end_time
+        end_time = time.time() + 3
+        is_wait = True # to enter reset timer
+        print("{}".format(text))
+        window['-CODE-'].update(code, text_color="#ff0")
+        window['-LABEL-'].update(label)
+        window['-PRICE-'].update(price, font=my_font_small, text_color="#fff")
+        window['-IMAGE-'].update(visible=False)
+    elif text:
         # add 3 sec on end_time
         end_time = time.time() + 3
         is_wait = True # to enter reset timer
@@ -91,12 +121,14 @@ while True:
         window['-CODE-'].update(code, font=my_font_small)
         window['-LABEL-'].update(label)
         window['-PRICE-'].update(price)
+        window['-IMAGE-'].update(visible=False)
 
     # reset timer
     if is_wait and time.time() > end_time:
-        window['-CODE-'].update(SCAN_HERE[0], font=my_font)
-        window['-LABEL-'].update(SCAN_HERE[1], font=my_font)
-        window['-PRICE-'].update(SCAN_HERE[2], font=my_font)
+        window['-CODE-'].update(SCAN_HERE[0], font=my_font, text_color="#fff")
+        window['-LABEL-'].update(SCAN_HERE[1], font=my_font, text_color="#fff")
+        window['-PRICE-'].update(SCAN_HERE[2], font=my_font, text_color="#ff0")
+        window['-IMAGE-'].update(visible=True)
         is_wait = False
     # reset vals
     text = ""
@@ -109,4 +141,6 @@ while True:
         if sg.popup_ok_cancel("Exit the program?", keep_on_top=True) == "OK":
             break
 
+# close all
+ser.close()
 window.close()
