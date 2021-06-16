44199cfa7282e5dad9b3535325e9084a7bf04dd0
TheMysteryz <zhuroland@hotmail.com>
2021-06-16 11:26:01 -0400

    [v0.7.6]: rm test

--- serial-ui.py
+++ serial-ui.py
@@ -49,7 +49,7 @@ w, h = sg.Window.get_screen_size()
 
 # defines
 BG_COLOR = "#00A2E8"
-SCAN_HERE = ["SCANNEZ VOS ARTICLES", "ICI test", ""]
+SCAN_HERE = ["SCANNEZ VOS ARTICLES", "ICI", ""]
 aa = ""
 my_font = "Arial 58"
 my_font_small = "Arial 29"
