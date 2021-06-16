d5489f3c53e3f13ebdb47316a81e4ea80629cda3
TheMysteryz <zhuroland@hotmail.com>
2021-06-16 11:49:30 -0400

    [v0.7.8]: sh

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!bin/bash
-cd /home/pi/python-usb &&
-git fetch &&
-git pull &&
+git --git-dir=/home/pi/python-usb/.git/ --work-tree=/home/pi/python-usb/ fetch &&
+git --git-dir=/home/pi/python-usb/.git/ --work-tree=/home/pi/python-usb/ pull &&
 python3 serial-ui.py
